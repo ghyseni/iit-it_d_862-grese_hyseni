ae74cd4e228c4df99defe34a2a139b61422facb5
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-09-05 19:54:35 -0500

    Add new comment from the comment input; Fade in the added comment

--- exercises/week3/js/app.js
+++ exercises/week3/js/app.js
@@ -2,11 +2,33 @@ var main = function() {
   "use strict";
   // window.alert("hello world");
 
+  //Add click event listener to add text from comment input to comments section
   $(".comment-input button").on("click", function() {
-    console.log("Hello world");
-    var $newP = $("<p>Added comment</p>");
-    $('.comments').append($newP);
+    addCommentFromInputBox();
   });
+
+  //Submit new comment add in Enter key press
+  $(".comment-input input").on("keypress", function(event) {
+    if (event.keyCode === 13) {
+      addCommentFromInputBox();
+    }
+  });
+
+  var addCommentFromInputBox = function() {
+    var $new_comment = $("<p>"),
+      $comment_text = $(".comment-input input");
+    if ($comment_text.val() !== "") {
+      $new_comment.text($comment_text.val());
+        $new_comment.hide();
+      $('.comments').append($new_comment);
+      $new_comment.fadeIn();
+
+      $comment_text.val("");
+    }
+
+  }
 }
+
+
 
 $(document).ready(main);
