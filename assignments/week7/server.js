a9d0afb9a1a9c498a33c79882b6522dde07c9b43
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-10-10 16:00:38 -0500

    Get request; get hand by id. Return 200 if found or 404 if not found.

--- assignments/week7/server.js
+++ assignments/week7/server.js
@@ -23,9 +23,22 @@ app.post('/hands',function(req,res){
 	hand.cards = req.body;
 	hand.save().then(function(doc){
 		res.status(200);
-		res.json({status:200,id:doc._id})
+		res.json({"status":200,"id":doc._id})
 	},function(err){
 		res.send(err);
+	});
+});
+
+/*
+* Get hand json object by giving the id to url
+* Return 200 status if hand is found
+* Otherwise, return 404 and error
+*/
+app.get("/hands/:handId",function(req,res){
+	Hand.findById(req.params.handId).then(function(hand){
+		res.send({id: hand._id, cards: hand.cards});
+	},function(err){
+		res.status(404).send({"status":404,"error":err});
 	});
 });
 
