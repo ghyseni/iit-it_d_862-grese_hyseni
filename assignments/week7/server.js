616cc14d4f62a22569f62867a5881083abd6c036
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-10-10 15:51:08 -0500

    Define hand model schema

--- assignments/week7/server.js
+++ assignments/week7/server.js
@@ -6,6 +6,15 @@ const port = 3000;
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+/* Create the hand model/schema */
+var Hand = mongoose.model('Hand',{
+	cards:{
+		type:[]
+	}
+});
+
+
+
 app.listen(port,function(){
 	console.log(`Started and running on port: ${port}`);
 });
