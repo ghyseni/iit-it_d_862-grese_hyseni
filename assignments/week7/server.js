9bc1ee1cacab517babc972270ab6b5ecd705e030
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-10-10 15:49:52 -0500

    Connect to databse via mongoose

--- assignments/week7/server.js
+++ assignments/week7/server.js
@@ -3,6 +3,9 @@ var express = require("express"),
     app = express(),
 const port = 3000;
 
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 app.listen(port,function(){
 	console.log(`Started and running on port: ${port}`);
 });
